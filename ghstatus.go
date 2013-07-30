efefb39e80b8fc24a44d3b53eec6a0e47e398d51
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-07-30 14:40:51 +0200

    First API implementation

--- ghstatus.go
+++ ghstatus.go
@@ -7,6 +7,11 @@ import (
 	"net/http"
 )
 
+type Status struct {
+	Status      string `json:"status"`
+	LastUpdated string `json:"last_updated"`
+}
+
 type Message struct {
 	Status    string `json:"status"`
 	Body      string `json:"body"`
@@ -22,21 +27,68 @@ func sendRequest(endpoint string) ([]byte, error) {
 	return ioutil.ReadAll(resp.Body)
 }
 
-func main() {
-	log.SetFlags(0)
+func GetStatus() (*Status, error) {
+	body, err := sendRequest("status")
+	if err != nil {
+		return nil, err
+	}
 
+	var status *Status
+	err = json.Unmarshal(body, &status)
+	if err != nil {
+		return nil, err
+	}
+	return status, nil
+}
+
+func GetMessages() ([]Message, error) {
 	body, err := sendRequest("messages")
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	var messages []Message
 	err = json.Unmarshal(body, &messages)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+	return messages, nil
+}
+
+func GetLastMessage() (*Message, error) {
+	body, err := sendRequest("last-message")
+	if err != nil {
+		return nil, err
 	}
 
+	var message *Message
+	err = json.Unmarshal(body, &message)
+	if err != nil {
+		return nil, err
+	}
+	return message, nil
+}
+
+func main() {
+	log.SetFlags(0)
+
+	status, err := GetStatus()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%+v", status)
+
+	messages, err := GetMessages()
+	if err != nil {
+		log.Fatal(err)
+	}
 	for _, m := range messages {
 		log.Printf("%+v", m)
 	}
+
+	message, err := GetLastMessage()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%+v", message)
 }
