bf8322d70137d417305cf41bf080db360f739c21
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-07-31 15:25:11 +0200

    Use httptest to fake web requests

--- ghstatus_test.go
+++ ghstatus_test.go
@@ -1,6 +1,66 @@
 package ghstatus
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var fakeResponses = []struct {
+	method, endpoint, body string
+}{
+	{
+		"GET",
+		"/api/status.json",
+		`{"status":"good","last_updated":"2013-07-31T12:09:46Z"}`,
+	},
+	{
+		"GET",
+		"/api/messages.json",
+		`[{
+			"body": "Everything operating normally.",
+			"created_on": "2013-07-29T22:23:19Z",
+			"status": "good"
+		},
+		{
+			"body": "We are continuing to work on the increased exception rate on the GitHub API. We will update again as soon as have the source of these exceptions resolved. ",
+			"created_on": "2013-07-29T21:09:54Z",
+			"status": "minor"
+		},
+		{
+			"body": "We've mitigated the DDoS attack and the site should responding normally. We're still investigating the cause of the small increase in exceptions when accessing the GitHub API.",
+			"created_on": "2013-07-29T16:10:54Z",
+			"status": "minor"
+		},
+		{
+			"body": "We're currently experiencing a large DDoS attack. The site is experiencing major packet loss and is mostly unavailable. We're working to further mitigate the attack.",
+			"created_on": "2013-07-29T15:05:38Z",
+			"status": "major"
+		},
+		{
+			"body": "We're investigating a small increase in exceptions affecting the GitHub API.",
+			"created_on": "2013-07-29T13:29:24Z",
+			"status": "minor"
+		}]`,
+	},
+	{
+		"GET",
+		"/api/last-message.json",
+		`{"status":"good","body":"Everything operating normally.","created_on":"2013-07-29T22:23:19Z"}`,
+	},
+}
+
+func init() {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for _, f := range fakeResponses {
+			if r.Method == f.method && r.URL.Path == f.endpoint {
+				fmt.Fprint(w, f.body)
+			}
+		}
+	}))
+	StatusApiUrl = ts.URL
+}
 
 func TestGetStatus(t *testing.T) {
 	status, err := GetStatus()
